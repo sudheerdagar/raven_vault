032012e3d7ad08629e5bc8a9bbb7dd0655aff724
Anudeepexperiment <anudeep0306@gmail.com>
2023-01-24 12:13:34 +0530

    created basic structure for react project

---
+++ my-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <h1 className='font-semibold'>React App</h1>
+    </div>
+  )
+}
+
+export default App
